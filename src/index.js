8e31b69885ec30214c790d3edd9084f8073fb038
SakumaSho <piki.0147@gmail.com>
2021-05-09 02:19:07 +0900

    :tada:　Help の実装

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const token = process.env.TOKEN
 const bot = new Client()
+const prefix = 'g:'
 
 bot.login(token)
     .catch(console.error);
@@ -11,4 +12,17 @@ bot.on('ready', () => {
     console.log(`${bot.user.username}を起動しました。`)
     bot.user.setActivity("Googleの正しい発音", { type: 'LISTENING' })
         .catch(console.error)
+})
+
+bot.on('message', (message) => {
+    if(!message.content.startsWith(`${prefix}`)) return;
+    if(message.content === `${prefix}help`) {
+        message.channel.send([
+            '```asciidoc',
+            '= GoogleWoman Help =',
+            'g:help :: これです。',
+            'g:gururu :: 『Googleの正しい発音』を再生します。',
+            '```'
+        ])
+    }
 })
